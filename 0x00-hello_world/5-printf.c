296da7a7cfcdd7cc9f23c305df92edb90d8f6aaa
SomaBosco95 <orahmaria261@gmail.com>
2023-03-16 15:20:49 -0700

    5 printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.>
+/**
+ * main - A program that a line using the ptintf funtion
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n")
+		return (0);
+}
