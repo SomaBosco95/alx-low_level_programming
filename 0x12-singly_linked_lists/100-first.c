024a75354e2f23b9d89a45b9e6dfbc8fdc603bca
Johnbosco S orah <orahmaria261@gmail.com>
2023-04-27 11:52:18 -0700

    100 first update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,7 @@
 /**
  * print_before_main - function that Prints a message before the main function is executed
  */
-void print_before_main(void)__attribute__((constructor));
+void print_before_main(void) __attribute__((constructor));
 void  print_before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
